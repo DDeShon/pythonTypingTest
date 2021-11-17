696d0d23a4abf72df70fa118ef6e15a68d97ed49
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-11-17 09:51:41 -0600

    set up function to capture text

--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,46 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(1, 0, "Welcome to the Typing Speed Test!")
+    stdscr.addstr("\n\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for the app!"
+    current_text = []
+
+
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr("Hello world!", curses.color_pair(1))
-    stdscr.addstr("Hello world!", curses.color_pair(2))
-    stdscr.addstr("Hello world!", curses.color_pair(3))
-    stdscr.refresh()
-    stdscr.getkey()
+
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
 
 wrapper(main)
